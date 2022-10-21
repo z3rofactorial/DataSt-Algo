541cf2261daf33605c6eaeb62b9d52e5abe25139
Shaurya Shrivastava <54743877+z3rofactorial@users.noreply.github.com>
2022-10-21 08:18:46 +0530

    Updates to Linear Search

--- binary_search.py
+++ binary_search.py
@@ -1,14 +1,24 @@
 # we used a dictionary to pass the input for easier testing
 test={
     'input':{
-        'cards':[13,11,10,6,4,3,1,0],
+        'cards':[13,11,7,6,4,3,1,3],
         'query':7
     },
-    'output':3
+    'output':2
 }
 
 
 # in order to prepare the edge test cases, We would create a list and then add multiple test cases for us to check
+'''
+[{'input': {'cards': [13,11,7,6,4,3,1,3], 'query': 7}, 'output': 2}, 
+{'input': {'cards': [13, 11, 10, 2, 4, 3, 4, 7], 'query': 7}, 'output': 7}, 
+{'input': {'cards': [13, 11, 10, 2, 4, 3, 1, 7], 'query': 2}, 'output': 3}, 
+{'input': {'cards': [], 'query': 1}, 'output': -1}, 
+{'input': {'cards': [3], 'query': 3}, 'output': 0}, 
+{'input': {'cards': [13, 11, 10, 2, 4, 2, 1, 7], 'query': 2}, 'output': 5}, 
+{'input': {'cards': [1, 1, 1, 1, 1, 1, 1, 1], 'query': 1}, 'output': 6}, 
+{'input': {'cards': [3, 3, 3, 3, 3, 3, 3, 3], 'query': 2}, 'output': -1}]
+'''
 test_cases = []
 test_cases.append(test)
 
@@ -40,7 +50,7 @@ test_cases.append({
 test_cases.append({
     'input':{
         'cards':[3],
-        'query':3
+        'query':2
     },
     'output':0
 })
@@ -70,22 +80,24 @@ test_cases.append({
 })
 
 
-def binary_search(cards,query):
+def linear_search(cards,query):
     i=0
-    while True:
-        if cards[i]== query:
-            return i
+    while i<len(cards):
+        if cards[i]==query:
+            pos=f'The position of {query} in {cards} is {i}\n'
+            return pos
         i=i+1
-
+    return -1
         
-# binary_search(test['input']['cards'],test['input']['query'])==test['output']
+# linear_search(test['input']['cards'],test['input']['query'])==test['output']
 # instead of this we can use,
-# binary_search(**test['input']) == test['output']
+# linear_search(**test['input']) == test['output']
 
-# if (binary_search(**test['input']) == test['output'])==test['output']:
+# if (linear_search(**test['input']) == test['output'])==test['output']:
 #     print("yes")
 # else:
 #     print("NO")
-
-result=binary_search(test['input']['cards'],test['input']['query'])
-print(result)
+for p in range(0,8):
+    result=linear_search(test_cases[p]['input']['cards'],test_cases[p]['input']['query'])
+    print(result)
+    p+=1
